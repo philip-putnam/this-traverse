411c22e8aec175f4b0fa37e3acd401e5e331f72d
philip putnam <putnamp989@gmail.com>
2017-01-24 21:56:58 -0800

    add back-end data, array of Movie objects, forEach loop to append movies in html list, click handler to turn each <li> movie item into button

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function Movie(movieName, rating, showtimes, release) {
+  this.movieName = movieName;
+  this.rating = rating;
+  this.showtimes = showtimes;
+  this.release = release;
+}
+
+var split = new Movie("Split", "PG-13", [1845, 2145], 2017);
+var liveByNight = new Movie("Live by Night", "R", [1100, 2230], 2017);
+var patriotsDay = new Movie("Patriot's Day", "PG-13", [1230, 1600, 1915, 2220], 2017);
+var hiddenFigures = new Movie("Hidden Figures", "PG-13", [1000, 1145, 1500, 1815, 2130], 2017);
+var rogueOne = new Movie("Rogue One: A Star Wars Story", "PG-13", [1200, 1515, 1830], 2017);
+
+var moviesDatabase = [split, liveByNight, patriotsDay, hiddenFigures, rogueOne];
+
+// Back-end logic
+
+$(document).ready(function() {
+
+  moviesDatabase.forEach(function(movie) {
+    var newMovie = new Movie(movie.movieName, movie.rating, movie.showtimes, movie.release);
+    $(".container ul").append("<li><span class='movies'>" + movie.movieName + "</span></li>");
+
+    $(".movies").last().click(function() {
+      $("#movie-times h2").text(newMovie.movieName);
+      $(".movie-times").text(movie.showtimes);
+    });
+
+  });
+
+
+
+});
